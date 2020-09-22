7db80b704f3873cb66486cc65819d3f7f50f3ccb
jkan9 <jkan9@hawaii.edu>
2020-09-22 11:55:04 -1000

    update

---
+++ Lab 6/ex4.js
@@ -0,0 +1,7 @@
+n = 2;
+age = 20;
+z = 10;
+
+console.log(n = n + z);
+n=2;
+console.log(n += z);
---
+++ Lab 6/ex5.js
@@ -0,0 +1,6 @@
+first_name = 'Jonathan';
+last_name = 'Kan';
+
+full_name = `${first_name} ${last_name}`;
+
+console.log(full_name);
