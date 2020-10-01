fd5a080e40e7c0b488456c4e4d23b8ba08f0dddb
jkan9 <jkan9@hawaii.edu>
2020-10-01 12:09:00 -1000

    update

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,11 +1,13 @@
 age_count = 1; //start the age count
 age = 54;
 while(age_count < age){ 
-    if(age_count >= age/2){
-        console.log("I'm Old!");
-        break;
+    if( age_count >= age/2 ) {
+        console.log("Don't ask how old I Am!");
+        process.exit();
+    } else {
+        console.log(`age ${age_count}`);
     }
-console.log(`age ${age_count}`)
+
 age_count++;
 }
-console.log(`Jonathan is ${age_count} year old`)
+console.log(`Jonathan is` + age_count +  `year old`)
